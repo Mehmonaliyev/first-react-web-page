b0e7bacd133ecd339e8a9d6fd9b6746aa7e858f7
Ibratjon <exsample@gmail.com>
2022-11-09 07:39:09 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+
+import { React, useState } from 'react'
+ 
+export default function App() {
+  const [counter, setCounter] = useState(0);
+ 
+  //increase counter
+  const incr = () => {
+    setCounter(count => count + 1);
+  };
+ 
+  //decrease counter
+  const decr = () => {
+    setCounter(count => count - 1);
+  };
+ 
+  //reset counter 
+  const reset = () =>{
+    setCounter(0)
+  }
+ 
+  return (
+    <div className="counter">
+      <h1>React Counter</h1>
+      <span className="counter__output">{counter}</span>
+      <div className="btn__container">
+        <button className="control__btn" onClick={incr}>+</button>
+        <button className="control__btn" onClick={decr}>-</button>
+        <button className="reset" onClick={reset}>Reset</button>
+      </div>
+    </div>
+  );
+}
